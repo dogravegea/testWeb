6356a10a8bfc9d1a93706b5107f44c38aee8a4f1
Dorian GRAVEGEAL <dorian.gravegeal@gmail.com>
2018-12-09 11:00:36 +0100

    Adaptation au serveur clevercloud

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
-app.use('/static', express.static(__dirname + '/images'));
+app.use('/static', express.static(process.env.BUCKET_HOST + '/images'));
 
 // Add a bit of logging
 app.use(morgan('short'))
@@ -72,7 +72,7 @@ app.get('/LierAEnclos/:id', function (req, res) {
 app.post('/addImg/:id', upload.single('file'), function (req, res) {
     if (req.file != undefined) {
         let ext = req.file.originalname.substring(req.file.originalname.lastIndexOf('.'), req.file.originalname.length);
-        let urlDestination = __dirname + "/images/" + req.params.id + ext;
+        let urlDestination = process.env.BUCKET_HOST + '/images/' + req.params.id + ext;
         //var imageData = fs.readFileSync(req.file.path);
         //fs.writeFileSync("src/test/" + req.params.id + ".png", imageData);
 
@@ -386,7 +386,7 @@ models.sequelize.sync().then(function() {
    * 
    * Listen only when database connection is sucessfull
    */
-  app.listen(3000, function() {
+  app.listen(process.env.PORT, function() {
     console.log('Express server listening on port 3000');
   });
 });
