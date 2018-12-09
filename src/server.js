2ff3a1c4c6fc64561887d4de2aa6e5119c47d5de
Dorian GRAVEGEAL <dorian.gravegeal@gmail.com>
2018-12-09 15:56:33 +0100

    modif fs bucket

--- src/server.js
+++ src/server.js
@@ -19,8 +19,8 @@ app.use(bodyParser.json())
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 
-process.env.CC_FS_BUCKET = '/:' + process.env.BUCKET_HOST;
-app.use('/static', express.static('/'));
+process.env.CC_FS_BUCKET = '/some/empty/folder:' + process.env.BUCKET_HOST;
+app.use('/static', express.static('/some/empty/folder'));
 
 // Add a bit of logging
 app.use(morgan('short'))
